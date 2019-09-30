12dcb8a8fedd7e81a92141790a68adc8e67f36d7
Diego Martinez <dmartinezm@gmail.com>
2019-09-30 01:54:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,11 +1,16 @@
 
+<<<<<<< HEAD
 def map(source_array)
+=======
+def map(source_array,block)
+>>>>>>> 79e814cbfec4888806d66ac548b18509ee87e1f8
   noo_array=[]
   source_array.each do |elem|
    noo_array << yield(elem)
   end
   noo_array
 end
+<<<<<<< HEAD
 
 def reduce(source_array, starting_point=nil)
 
@@ -23,3 +28,5 @@ def reduce(source_array, starting_point=nil)
 
  total
 end
+=======
+>>>>>>> 79e814cbfec4888806d66ac548b18509ee87e1f8
